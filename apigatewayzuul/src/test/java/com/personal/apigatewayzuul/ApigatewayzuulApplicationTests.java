f290573940612aecf392d67be6466d934226ca23
Paulo Daniel <pdanielcdo11@gmail.com>
2024-07-25 00:22:18 -0300

    separando e criando primeiros ms

---
+++ apigatewayzuul/src/test/java/com/personal/apigatewayzuul/ApigatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
